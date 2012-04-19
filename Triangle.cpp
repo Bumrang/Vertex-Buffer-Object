96eb3960cb4b84058e7c8b1314189921a617aa51
Bumrang <Maxim@Maxim-PC.(none)>
2012-04-18 21:38:54 -0700

    hi facepunch WDYNHW

--- Triangle.cpp
+++ Triangle.cpp
@@ -2,7 +2,7 @@
 
 void Triangle::Load()
 {
-	GLfloat Verts[] = {0, 2, 0, /**/ -2, -2, 0, /**/  2, -2, 0}; // all the verts
+	GLfloat Verts[] = {800 / 2, 600, 0, /**/ 0, 0, 0, /**/  800, 0, 0}; // all the verts
 	GLubyte Index[] = {0, 1, 2}; // index
 
 	glGenBuffers (1 , &VBO); // create the buffers
@@ -17,11 +17,12 @@ void Triangle::Load()
 
 void Triangle::Draw()
 {
+	glPushMatrix();
 	glLoadIdentity ();
 	glTranslatef (0 , 0 , -10);
 	glRotatef (ZRot , 0 , 0 , 1);
 
-	glColor3f (0 , 1 , 0);
+	glColor3f (1 , 0 , 0);
 
 	glBindBuffer (GL_ARRAY_BUFFER , VBO);
 	glBindBuffer (GL_ELEMENT_ARRAY_BUFFER , Indices);
@@ -30,4 +31,20 @@ void Triangle::Draw()
 	glVertexAttribPointer (0 , 3 , GL_FLOAT , GL_FALSE , sizeof (float) * 3 , 0);
 
 	glDrawElements (GL_TRIANGLES , 3 , GL_UNSIGNED_BYTE , 0);
+
+	glPopMatrix();
+
+/*	glPushMatrix();
+	glTranslatef(0,0,0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(800/2,600);
+		glVertex2f(0,0);
+		glVertex2f(800,0);
+	glEnd();
+	glPopMatrix();*/  // immediate mode, use if vbo is not working (debugging yay)
+
+
+
+	
+
 }
--- main.cpp
+++ main.cpp
@@ -13,16 +13,19 @@ int main()
       std::cout << "GLFW failed to initialize!\n";
       return 1;
    }
-   glfwOpenWindow(Width,Height,0,0,0,0,0,0,GLFW_WINDOW); // open the window 
-   glfwSetWindowTitle("VBO"); // title
+   glfwOpenWindow(Width,Height,8,8,8,0,0,0,GLFW_WINDOW); // open the window 
+   glfwSetWindowTitle("Vertex Buffer Object Test"); // title
    glViewport(0,0,Width, Height); // stuff
    glMatrixMode(GL_PROJECTION); // matrix mode
    glLoadIdentity(); // stuff
    glOrtho(0,Width,0,Height,0,1); // stuff
    glMatrixMode(GL_MODELVIEW); // matrix mode
-   glClearColor(0,0,0,1); // back color
+   glClearColor(0,0,1,1); // back color
 
-   glewInit();
+   if (glewInit() != GLEW_OK)
+   {
+	   std::cout << "Could not load GLEW :(";
+   }
 
    Triangle Tri;
 
@@ -31,7 +34,7 @@ int main()
 	while (true)
 	{
 
-		if (!glfwGetWindowParam(GLFW_OPENED)) // Closed the window? That's pretty cool.
+		if (!glfwGetWindowParam(GLFW_OPENED))
 		 {
            std::cout << "Window was closed! Exiting...\n";
            break; 
@@ -40,7 +43,7 @@ int main()
 		Tri.ZRot += 0.5;
 
 		// Updates above clear.
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		// Draws under clear.
 	
 		Tri.Draw();
